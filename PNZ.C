51dfc9b2a5356354663b1d704f361a960f85a48e
kavikarthick <35654130+kavikarthick@users.noreply.github.com>
2018-01-23 16:55:02 +0530

    Create PNZ.C

---
+++ PNZ.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int a=5;
+if(a>0)
+printf("%d,the given number is positive");
+else if(a<0)
+printf("%d,the given number is negative");
+else
+printf("%d,the given number is zero");
+}
