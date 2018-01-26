ac6a5129059bef0dd3a8e83c6dc95fd81ddcbb22
kavikarthick <35654130+kavikarthick@users.noreply.github.com>
2018-01-26 05:10:45 +0000

    Create kavi.c

---
+++ kavi.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main()
+{
+int year;
+
+printf("Enter a year\n");
+scanf("%d",&year);
+
+if ( (year % 4) == 0)
+printf("%d is a leap year",year);
+else
+printf("%d is not a leap year\n",year);
+}
+
