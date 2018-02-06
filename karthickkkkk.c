043777b8d9f8e12ff80d1f9baa5519135ae2782d
kavikarthick <35654130+kavikarthick@users.noreply.github.com>
2018-02-06 16:39:59 +0530

    Create karthickkkkk.c

---
+++ karthickkkkk.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    scanf(" %d %d %d",&a,&b,&c);
+    if(a>b && a>c)
+    {
+    printf("a is  largest %d",a);
+    }
+    if else(b>c)
+    {
+    printf(" b is largest %d",b);
+    }
+    else
+    {
+    printf(" c is larest %d",c);
+    }
+    
+}
