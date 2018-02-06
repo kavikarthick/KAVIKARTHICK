7f0ac2d647418eaf318f9bba5b17c62179231eb8
kavikarthick <35654130+kavikarthick@users.noreply.github.com>
2018-02-06 16:56:29 +0530

    Create kviiiiiii.c

---
+++ kviiiiiii.c
@@ -0,0 +1,17 @@
+#include<conio.h>
+void main()
+{
+	clrscr();
+	char ch;
+	cout<<"Enter a character: ";
+	cin>>ch;
+	if((ch>='a'&& ch<='z') || (ch>='A' && ch<='Z'))
+	{
+		cout<<ch<<" is an alphabet";
+	}
+	else
+	{
+		cout<<ch<<" is not an alphabet";
+	}
+	getch();
+}
