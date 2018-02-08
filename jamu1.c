6437c4ee0f8585d568163692f77bfc50417ebb45
kavikarthick <35654130+kavikarthick@users.noreply.github.com>
2018-02-08 16:40:29 +0530

    Create jamu1.c

---
+++ jamu1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void main()
+{
+    int a,b,c;
+    int array[5];
+    printf("enter the first number");
+    printf("enter a number");
+    scanf("%d%d",&a,&b);
+    printf("enter a array");
+    for(int i=0;i<5;i++)
+    {
+        scanf("%d",&array[i]);
+    }
+    for(int j=0;j<a;j++)
+{
+    c+=array[j];
+}
+ 
+    printf("%d",c);
+}
