20fdbe1ea4e28df5c7786cc39bc0eb64d9e1d949
kavikarthick <35654130+kavikarthick@users.noreply.github.com>
2018-02-09 16:29:20 +0530

    Create k6.c

---
+++ k6.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int i,a,b;
+    printf(" Enter the number");
+    scanf("%d %d",&a,&b);
+    for(i=a;i<=b;i++)
+    {
+        if(i%2==0)
+    printf("%d",i);
+        
+    }
+}
