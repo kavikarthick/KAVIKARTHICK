0e820912ca59b3dc99dd10457eaad28c08e9710d
kavikarthick <35654130+kavikarthick@users.noreply.github.com>
2018-03-20 16:53:43 +0530

    Create strcat() function.c

---
+++ strcat() function.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+   char k[20],g[20];
+   printf(" Enter two string");
+   scanf("%s%s",&k,&g);
+   printf("%s%s",k,g);
+    
+
+        return 0;
+}
